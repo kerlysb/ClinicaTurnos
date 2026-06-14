2ed4547223ed5ca5d0dfac328458fc3e7a52424c
Kerly Suárez <ks.suarezb@uea.edu.ec>
2026-06-14 18:10:24 -0500

    Clase constantes

---
+++ Modelos.cs
@@ -0,0 +1,11 @@
+namespace ClinicaTurnos
+{
+    public static class Constantes
+    {
+        public const int MAX_PACIENTES = 50;
+        public const int MAX_TURNOS    = 100;
+        public const int MAX_DOCTORES  = 10;
+        public const int DIAS_SEMANA   = 7;
+        public const int HORAS_DIA     = 10;
+    }
+    
