2431a5fe48616298e194880146e4975bdd8e9234
Kerly Suárez <ks.suarezb@uea.edu.ec>
2026-06-16 21:57:52 -0500

    Metodos Eliminar y Buscar

--- GestorPacientes.cs
+++ GestorPacientes.cs
@@ -28,4 +28,31 @@ namespace ClinicaTurnos
             _pacientes[_total++] = p;
             return true;
         }
-        
+        public bool Eliminar(int id)
+        {
+            for (int i = 0; i < _total; i++)
+            {
+                if (_pacientes[i].Id == id)
+                {
+                    _pacientes[i].Activo = false;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Paciente? BuscarPorId(int id)
+        {
+            for (int i = 0; i < _total; i++)
+                if (_pacientes[i].Id == id && _pacientes[i].Activo)
+                    return _pacientes[i];
+            return null;
+        }
+
+        public Paciente? BuscarPorCedula(string cedula)
+        {
+            for (int i = 0; i < _total; i++)
+                if (_pacientes[i].Cedula == cedula && _pacientes[i].Activo)
+                    return _pacientes[i];
+            return null;
+        }
